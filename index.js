1875df551f46a482079ff26bf42ec55fc715c3a6
skandarajeev <skandarajeev@gmail.com>
2023-09-27 13:24:30 +0530

    corse error

--- index.js
+++ index.js
@@ -46,6 +46,14 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "daga.vercel.app"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(ignoreFavicon);
 app.listen(process.env.PORT);
 
