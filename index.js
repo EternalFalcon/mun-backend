99d4c4d22c5f49191b6927a031762174ae658972
skandarajeev <skandarajeev@gmail.com>
2023-09-27 13:30:04 +0530

    oyo

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "daga.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://daga.vercel.app"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
